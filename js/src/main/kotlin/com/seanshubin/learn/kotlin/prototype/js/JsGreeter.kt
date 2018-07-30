0f6d825b8a41f417ccefd1182171822b4ddb2546
Sean Shubin <sshubin@cj.com>
2018-07-30 12:44:43 -0700

    made delegate private

--- js/src/main/kotlin/com/seanshubin/learn/kotlin/prototype/js/JsGreeter.kt
+++ js/src/main/kotlin/com/seanshubin/learn/kotlin/prototype/js/JsGreeter.kt
@@ -2,6 +2,6 @@ package com.seanshubin.learn.kotlin.prototype.js
 
 import com.seanshubin.learn.kotlin.prototype.common.CommonGreeter
 
-class JsGreeter(commonGreeter:CommonGreeter) {
+class JsGreeter(private val commonGreeter:CommonGreeter) {
     fun greet(target:String):String = commonGreeter.greet(target)
 }
--- jvm/src/main/kotlin/com/seanshubin/learn/kotlin/prototype/jvm/JvmGreeter.kt
+++ jvm/src/main/kotlin/com/seanshubin/learn/kotlin/prototype/jvm/JvmGreeter.kt
@@ -2,6 +2,6 @@ package com.seanshubin.learn.kotlin.prototype.jvm
 
 import com.seanshubin.learn.kotlin.prototype.common.CommonGreeter
 
-class JvmGreeter(commonGreeter:CommonGreeter) {
+class JvmGreeter(private val commonGreeter:CommonGreeter) {
     fun greet(target:String):String = commonGreeter.greet(target)
 }
